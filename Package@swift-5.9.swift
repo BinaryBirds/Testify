1bef08166d423b156b55e8ac90a32c69805d08cc
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2024-10-09 06:38:19 +0200

    feat: update for swift

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "testify",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    products: [
+        .executable(name: "testify", targets: ["testify"]),
+        .library(name: "TestifySDK", targets: ["TestifySDK"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "testify",
+            dependencies: [
+                .target(name: "TestifySDK")
+            ]
+        ),
+
+        // MARK: - targets
+
+        .target(
+            name: "TestifySDK",
+            dependencies: []
+        ),
+        
+        // MARK: - test targets
+
+        .testTarget(
+            name: "TestifySDKTests",
+            dependencies: [
+                .target(name: "TestifySDK"),
+            ],
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+    ]
+)
